32d3920e196416f61f9373c2c52eca6781751079
Andrei Costea <ascostea@gmail.com>
2021-08-11 11:20:35 +0300

    rogue

--- src/scripts/objects/rogue.ts
+++ src/scripts/objects/rogue.ts
@@ -1,5 +1,18 @@
 import 'phaser'
 
 export default class Rogue extends Phaser.GameObjects.Sprite { 
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, x, y, 'rogue');
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
     
+        (this.body as Phaser.Physics.Arcade.Body).setCollideWorldBounds(true);
+    
+        (this.body as Phaser.Physics.Arcade.Body).setSize(30, 54);
+        (this.body as Phaser.Physics.Arcade.Body).setOffset(70, 57);
+    
+        this.anims.play('rogue-idle-anim');
+      }
+    
+      preupdate() {}
 }
