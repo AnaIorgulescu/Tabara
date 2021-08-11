7bd5cb05683899735f43077de3073e15f144ba45
Ana Iorgulescu <anaiorgulescu12008@gmail.com>
2021-08-11 10:49:26 +0300

    not working

--- src/scripts/scenes/level1.ts
+++ src/scripts/scenes/level1.ts
@@ -1,4 +1,4 @@
-
+import Mage from '../objects/mage'
 
 export default class Level1 extends Phaser.Scene {
   fpsText
@@ -8,34 +8,21 @@ export default class Level1 extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('hero','assets/mage/mage.png');
-    this.load.spritesheet('idle-sprite' , 'assets/mage/idle.png' , {frameWidth:171,frameHeight:121});
+    this.load.image('mage-hero', 'assets/mage/mage.png')
+    this.load.spritesheet('mage-idle-sprite', 'assets/mage/idle.png', { frameWidth: 171, frameHeight: 121 })
   }
   create() {
     this.anims.create({
-      key:'idle-anim', 
-      frames:[
-        {frame:0,key:'hero', duration:5000},
-        ...this.anims.generateFrameNumbers('idle-sprite', {})
-      ],
+      key: 'mage-idle-anim',
+      frames: [{ frame: 0, key: 'hero', duration: 5000 }, 
+      ...this.anims.generateFrameNumbers('mage-idle-sprite', {})],
       frameRate: 6,
       repeat: -1
-    });
+    })
 
-    let hero = this.physics.add.sprite(171, 128, 'hero');
-    hero.body.setCollideWorldBounds(true);
-    
-    hero.body.setSize(30,54);
-    hero.body.setOffset(70,57);
-
-    hero.anims.play('idle-anim');
-
-    this.cameras.main.fadeIn();
-     
-
+    let hero = new Mage(this, 10, 10)
+    this.cameras.main.fadeIn()
   }
 
-  update() {
-   
-  }
+  update() {}
 }
