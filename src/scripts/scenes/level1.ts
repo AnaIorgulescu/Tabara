fca874ac377482c4cda1325059ace4baf4beda8e
Ana Iorgulescu <anaiorgulescu12008@gmail.com>
2021-08-11 12:44:23 +0300

    changes

--- src/scripts/scenes/level1.ts
+++ src/scripts/scenes/level1.ts
@@ -9,12 +9,19 @@ export default class Level1 extends Phaser.Scene {
   preload() {
     this.load.image('mage-hero', 'assets/mage/mage.png')
     this.load.spritesheet('mage-idle-sprite', 'assets/mage/idle.png', { frameWidth: 171, frameHeight: 121 })
+    this.load.spritesheet('mage-walk-sprite', 'assets/mage/walk.png', { frameWidth: 171, frameHeight: 121 })
   }
   create() {
     this.anims.create({
       key: 'mage-idle-anim',
       frames: [{ frame: 0, key: 'mage-hero', duration: 5000 }, 
       ...this.anims.generateFrameNumbers('mage-idle-sprite', {})],
+      frameRate: 6,
+      repeat: -1
+    })
+    this.anims.create({
+      key: 'mage-walk-anim',
+      frames: this.anims.generateFrameNumbers('mage-walk-sprite', {}),
       frameRate: 6,
       repeat: -1
     })
