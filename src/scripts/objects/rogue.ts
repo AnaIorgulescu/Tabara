41bdda30391e09df3ed5fa049cb1d4620746f907
Andrei Costea <ascostea@gmail.com>
2021-08-12 11:34:11 +0300

    jump

--- src/scripts/objects/rogue.ts
+++ src/scripts/objects/rogue.ts
@@ -4,6 +4,7 @@ export default class Rogue extends Phaser.GameObjects.Sprite {
 
     rightKey:Phaser.Input.Keyboard.Key
     leftkey:Phaser.Input.Keyboard.Key
+    upkey:Phaser.Input.Keyboard.Key
     herostate: String
     animstate: String
 
@@ -17,32 +18,51 @@ export default class Rogue extends Phaser.GameObjects.Sprite {
         (this.body as Phaser.Physics.Arcade.Body).setSize(30, 54);
         (this.body as Phaser.Physics.Arcade.Body).setOffset(70, 57);
         (this.body as Phaser.Physics.Arcade.Body).setMaxVelocity(200,400);
-        (this.body as Phaser.Physics.Arcade.Body).setDragX(2000);
+        (this.body as Phaser.Physics.Arcade.Body).setDragX(25000);
     
         this.anims.play('rogue-idle-anim');
         this.herostate= 'idle'
         this.rightKey= scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D) 
         this.leftkey= scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        this.upkey= scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
             
         }
     
       preUpdate(time:number, delta:number) {
+        console.log('hi,you little boy');
           super.preUpdate(time, delta);
         if(this.rightKey.isDown) {
+            (this.body as Phaser.Physics.Arcade.Body).setMaxVelocity(300,500);
             (this.body as Phaser.Physics.Arcade.Body).setAccelerationX(300);
-            this. herostate = 'walk'
+            if((this.body as Phaser.Physics.Arcade.Body).onFloor() && (this.body as Phaser.Physics.Arcade.Body).velocity.y == 0) {
+                this. herostate = 'walk';
+            }
             this.setFlipX(false)
         }
         if(this.leftkey.isDown) {
             (this.body as Phaser.Physics.Arcade.Body).setMaxVelocity(300,500);
             (this.body as Phaser.Physics.Arcade.Body).setAccelerationX(-300);
             this.setFlipX(true)
-            this.herostate = 'walk'
+            if((this.body as Phaser.Physics.Arcade.Body).onFloor() && (this.body as Phaser.Physics.Arcade.Body).velocity.y == 0) {
+                this.herostate = 'walk';
+            }
         }
-        if(this.leftkey.isUp && this.rightKey.isUp){
+        if((this.body as Phaser.Physics.Arcade.Body).onFloor() && (this.body as Phaser.Physics.Arcade.Body).velocity.y == 0 && Phaser.Input.Keyboard.JustDown(this.upkey)) {
+            (this.body as Phaser.Physics.Arcade.Body).setVelocityY(-300);
+            this.herostate = 'jump'
+
+        }
+        if(this.leftkey.isUp && this.rightKey.isUp && (this.body as Phaser.Physics.Arcade.Body).onFloor() && (this.body as Phaser.Physics.Arcade.Body).velocity.y == 0){
           (this.body as Phaser.Physics.Arcade.Body).setAccelerationX(0)
           this.herostate = 'idle';
         }
+        if(this.herostate == 'jump'&& Phaser.Input.Keyboard.JustDown(this.upkey)){
+            (this.body as Phaser.Physics.Arcade.Body).setVelocityY(-500);
+            this.herostate = 'double-jump'
+        }
+
+
+
         if(this.herostate == 'walk' && this.animstate != 'walk'){
             this.anims.play('rogue-walk-anim');
             this.animstate = 'walk';
@@ -50,6 +70,14 @@ export default class Rogue extends Phaser.GameObjects.Sprite {
         if(this.herostate == 'idle' && this.animstate != 'idle'){
             this.anims.play('rogue-idle-anim');
             this.animstate = 'idle';
+        }
+        if(this.herostate == 'jump' && this.animstate != 'jump'){
+            this.anims.play('rogue-jump-anim');
+            this.animstate = 'jump';
+        }
+        if(this.herostate == 'double-jump' && this.animstate != 'double-jump'){
+            this.anims.play('rogue-double-jump-anim');
+            this.animstate = 'double-jump';
         }
       }
 }
--- src/scripts/scenes/level2.ts
+++ src/scripts/scenes/level2.ts
@@ -10,6 +10,8 @@ export default class Level2 extends Phaser.Scene {
     this.load.image('rogue-hero', 'assets/rogue/rogue.png')
     this.load.spritesheet('rogue-idle-sprite', 'assets/rogue/idle.png', { frameWidth: 171, frameHeight: 128 })
     this.load.spritesheet('rogue-walk-sprite', 'assets/rogue/walk.png', { frameWidth: 171, frameHeight: 128 })
+    this.load.spritesheet('rogue-jump-sprite', 'assets/rogue/jump.png', { frameWidth: 171, frameHeight: 128 })
+    this.load.spritesheet('rogue-double-jump-sprite', 'assets/rogue/double-jump.png', { frameWidth: 171, frameHeight: 128 })
   }
   create() {
     this.anims.create({
@@ -18,13 +20,27 @@ export default class Level2 extends Phaser.Scene {
       ...this.anims.generateFrameNumbers('rogue-idle-sprite', {})],
       frameRate: 6,
       repeat: -1
-    })
+    });
     this.anims.create({
       key: 'rogue-walk-anim',
       frames: this.anims.generateFrameNumbers('rogue-walk-sprite', {}),
       frameRate: 6,
       repeat: -1
-    })
+    });
+    this.anims.create({
+      key: 'rogue-jump-anim',
+      frames: this.anims.generateFrameNumbers('rogue-jump-sprite', {}),
+      frameRate: 6,
+      repeat: 0
+    });
+    this.anims.create({
+      key: 'rogue-double-jump-anim',
+      frames: this.anims.generateFrameNumbers('rogue-double-jump-sprite', {}),
+      frameRate: 20,
+      repeat: 0
+    });
+    
+
 
     let hero = new Rogue(this, 10, 10)
     this.cameras.main.fadeIn()
