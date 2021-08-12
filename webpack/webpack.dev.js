53036148c0661df1e7bd7a81a9655f29e0565da0
Mihai Vasilache <mihai.vasilache@edenred.com>
2021-08-12 22:11:30 +0300

    resize screen

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,13 +1,13 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 const dev = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    open: true,
-    port: 8081
-  }
-}
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        open: true,
+        port: 8081
+    }
+};
 
-module.exports = merge(common, dev)
+module.exports = merge(common, dev);
