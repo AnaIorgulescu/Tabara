f2e4aecd207bbd60a386d77f1c23d670c4fa7899
Ana Iorgulescu <anaiorgulescu12008@gmail.com>
2021-08-13 10:37:34 +0300

    spike

--- src/scripts/scenes/level1.ts
+++ src/scripts/scenes/level1.ts
@@ -85,7 +85,7 @@ export default class Level1 extends Phaser.Scene {
         let spikeGroup = this.physics.add.group({ immovable: true, allowGravity: false });
         for (let object of objects) {
             if (object.type == 'spike') {
-                let spike = spikeGroup.create(object.x, object.y, 'tileset-tiles', object.gid - tiles.firstgid);
+                let spike = spikeGroup.create(object.x, object.y, 'tileset-tiles', object.gid || 0 - tiles.firstgid);
             }
         }
 
