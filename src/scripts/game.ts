7c4a3310f471c49213acd52b8f0749a752114d11
Mihai Vasilache <mihai.vasilache@edenred.com>
2021-08-13 09:19:22 +0300

    debug

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -25,7 +25,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 400 },
-            debug: false,
+            debug: true,
             debugShowVelocity: true,
             debugShowBody: true,
             debugShowStaticBody: true
